24c52a09d8358d14b1466a8e3cddea419543cfca
garu <zanggaru@gmail.com>
2024-02-27 22:03:46 +0900

    first commit

---
+++ src/main/java/com/toy/mbtiservice/MbtiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.toy.mbtiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toy/mbtiservice/MbtiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.mbtiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
